b8ba2337220c375a09ca5613aaeef6a8bb8d6550
qiuzhanghua <qiuzhanghua@icloud.com>
2023-04-29 17:56:08 +0800

    <Dynamic> usage

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,22 @@
-import { createSignal, Show } from "solid-js";
+import { createSignal, For, Show } from "solid-js";
+import { Dynamic } from "solid-js/web";
+
+const RedThing = () => <strong style={{ color: "red" }}>Red Thing</strong>;
+const GreenThing = () => (
+  <strong style={{ color: "green" }}>Green Thing</strong>
+);
+const BlueThing = () => <strong style={{ color: "blue" }}>Blue Thing</strong>;
+
+const options = {
+  red: RedThing,
+  green: GreenThing,
+  blue: BlueThing
+};
 
 export default function About () {
   const [loggedIn, setLoggedIn] = createSignal(false);
   const toggle: () => boolean = () => setLoggedIn(!loggedIn());
+  const [selected, setSelected] = createSignal("red");
 
   return (
     <>
@@ -23,6 +37,17 @@ export default function About () {
           Logout
         </button>
       </Show>
+
+      <p />
+      <select
+        value={selected()}
+        onInput={(e) => setSelected(e.currentTarget.value)}
+      >
+        <For each={Object.keys(options)}>
+          {(color) => <option value={color}>{color}</option>}
+        </For>
+      </select>
+      <Dynamic component={options[selected()]} />
     </>
   );
 }
