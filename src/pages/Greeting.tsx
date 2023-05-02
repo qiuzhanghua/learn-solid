612774e60b106bfee282f4b78b344f5b732300db
qiuzhanghua <qiuzhanghua@icloud.com>
2023-05-02 15:03:09 +0800

    split properties

---
+++ src/pages/Greeting.tsx
@@ -0,0 +1,10 @@
+import { splitProps } from "solid-js";
+
+export default function Greeting (props) {
+  const [local, others] = splitProps(props, ["greeting", "name"]);
+  return (
+    <h3 {...others}>
+      {local.greeting} {local.name}
+    </h3>
+  );
+}
--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -1,4 +1,6 @@
 import Info from "./Info.tsx";
+import { createSignal } from "solid-js";
+import Greeting from "./Greeting.tsx";
 
 const pkg = {
   name: "solid-js",
@@ -7,6 +9,17 @@ const pkg = {
   website: "https://solidjs.com"
 };
 
+const [name, setName] = createSignal("Daniel");
+
 export default function () {
-  return <Info {...pkg} />;
+  return (
+    <>
+      <Info {...pkg} />
+      <p />
+      <Greeting greeting="你好" name={name()} style={{ color: "teal" }} />
+      <button class="btn" onClick={() => setName("邱张华")}>
+        设置中文名
+      </button>
+    </>
+  );
 }
